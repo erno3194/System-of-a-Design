499cabc6261619aa6bfb977c9d203e664100ddfe
pottfisk <robert.paananen@hotmail.com>
2020-02-10 18:55:43 +0100

    Fixed two buttons on start screen for användare

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,1 +1,25 @@
 /* JAVASCRIPT */
+
+const vm = new Vue({
+    el: '#main',
+    data: {
+	
+    },
+    methods: {
+	hideButtons: function() {
+	    var skapaButton = document.getElementById("skapaProfilButton");
+	    var tillButton = document.getElementById("tillProfilButton");
+	    skapaButton.style.display = "none";
+	    tillButton.style.display = "none";
+	},
+	skapaProfil: function() {
+	    console.log("Click");
+	    this.hideButtons();
+	},
+	tillProfil: function() {
+	    console.log("Click");
+	    this.hideButtons();
+	}
+    }
+    
+})
