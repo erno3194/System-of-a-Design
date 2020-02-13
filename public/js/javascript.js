e0e65ef155688c0a03bc6e16593dc6bc7828bab6
pottfisk <robert.paananen@hotmail.com>
2020-02-13 14:42:37 +0100

    Fixed button only working when everything is filled in, and added hobbies

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -7,7 +7,10 @@ const vm = new Vue({
 	email: "",
 	age: "",
 	gender: "male",
-	preferredAge: "18-25" 
+	preferredAge: "18-25",
+	hobbies: ["Sports", "Food", "Outdoors", "Fitness", "Movies", "Other"],
+	selectedHobbies: [],
+	reg: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/
     },
     methods: {
 	markDone: function(name, email, age, gender, preferredAge){
@@ -21,6 +24,28 @@ const vm = new Vue({
 	    console.log(age);
 	    console.log(gender);
 	    console.log(preferredAge);
+	    console.log(this.selectedHobbies);
+	    var skapaProfil = document.getElementById("skapaProfil");
+	    if(name && email && age && gender && preferredAge){
+		skapaProfil.style.display = "none";
+	    } else {
+		var button = document.getElementById("submitProfileButton");
+		button.style.color = "red";
+		setTimeout(function(){button.style.color = "black";},600);
+
+		if(!name){
+		    document.getElementById("nameParagraph").style.color = "red";
+		} else document.getElementById("nameParagraph").style.color = "black";
+		if(!this.reg.test(email) || !email){
+		    document.getElementById("emailParagraph").style.color = "red";
+		} else document.getElementById("emailParagraph").style.color = "black";
+		if(!age){
+		    document.getElementById("ageParagraph").style.color = "red";
+		} else document.getElementById("ageParagraph").style.color = "block";
+		if(this.selectedHobbies.length == 0){
+		    document.getElementById("intressenLabel").style.color = "red";
+		} else document.getElementById("intressenLabel").style.color = "black";
+	    }
 	},
 	hideButtons: function() {
 	    var skapaButton = document.getElementById("skapaProfilButton");
