ae6d8597542e22a23360025a635bcb58a2a13bf6
adamaxelsson <adam.axelsson.4529@student.uu.se>
2020-02-21 11:22:09 +0100

    förslag på lösning i vue

--- public/js/evaluation_vue.js
+++ public/js/evaluation_vue.js
@@ -3,21 +3,25 @@
 //var answers = {q1, q2, q3};
 
 const vm = new Vue({
-  el: "#submitFormButton",
-  data: {
-  methods: {
-    submitEval: function(){
-      console.log("TESTING TESTINGS")
+  el: "#evalFormDiv",
+    data: {
+	rating: "",
+	interests: "",
+	match: "",
+    },
+    methods: {
+	submitEval: function(match, interests, rating){
+	    console.log("TESTING TESTINGS")
+	    /*
+	      var matchRate = document.getElementById("submitFormButton").elements['matching'];
+	      var interestRate = document.getElementById("submitFormButton").elements['interest'];
+	      var overallRate = document.getElementById("submitFormButton").elements['allRate'];
+	    */
+	    console.log(match);
+	    console.log(interests);
+	    console.log(rating);
 
-      var matchRate = document.getElementById("submitFormButton").elements['matching'];
-      var interestRate = document.getElementById("submitFormButton").elements['interest'];
-      var overallRate = document.getElementById("submitFormButton").elements['allRate'];
-
-      console.log(matchRate);
-      console.log(interestRate);
-      console.log(overallRate);
-
+	}
+	
     }
-  }
-}
 })
